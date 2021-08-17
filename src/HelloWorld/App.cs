ef4eef0fe7cc691f40ee5a703222de6b65aaef8a
Meleg Zoltán <zoltan.meleg@generali.com>
2021-08-17 10:49:39 +0200

    tasks done 1-5

---
+++ src/HelloWorld/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codecool.HelloWorld
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                HelloWorld helloWorld = new HelloWorld();
+                helloWorld.Welcome(args[0]);
+            } else
+            {
+                Console.WriteLine("hasznalat: HelloWorld <nevargument>");
+            }
+        }
+    }
+}
--- src/make.ps1
+++ src/make.ps1
@@ -1,1 +1,4 @@
 # TODO
+dotnet build .\HelloWorld\HelloWorld.csproj
+$param1=$args[0]
+HelloWorld\bin\Debug\netcoreapp3.1\HelloWorld.exe $param1
